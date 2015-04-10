b1d320842de2fb770fff3485409d00d41110f08d
Andrew H. Yi <andrew.yi50@gmail.com>
2015-04-10 00:03:44 -0400

    Create RecordedAudio model class

---
+++ ReocrdAudio.swift
@@ -0,0 +1,14 @@
+//
+//  ReocrdAudio.swift
+//  Pitch Perfect
+//
+//  Created by Andrew Yi on 4/10/15.
+//  Copyright (c) 2015 AndrewHYi. All rights reserved.
+//
+
+import Foundation
+
+class RecordedAudio: NSObject {
+    var filePathUrl: NSURL!
+    var title: String!
+}
